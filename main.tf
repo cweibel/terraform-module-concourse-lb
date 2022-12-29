121516e3194d64d9f07682b114e6e75d17271806
Chris Weibel <christopher.weibel@gmail.com>
2022-12-29 14:06:20 -0500

    Update main.tf
    Change ALB to LB

--- main.tf
+++ main.tf
@@ -13,24 +13,24 @@ variable enable_route_53       { default = 1 }  # Disable if using CloudFlare or
 ################################################################################
 # Concourse ALB
 ################################################################################
-resource "aws_lb" "concourse_alb" {
-  name               = "concourse-alb"
+resource "aws_lb" "concourse_lb" {
+  name               = "concourse-lb"
   internal           = var.internal_lb
   load_balancer_type = "application"
   subnets            = var.subnet_ids
   security_groups    = var.security_groups
-  tags               = merge({Name = "concourse-alb"}, var.resource_tags)
+  tags               = merge({Name = "concourse-lb"}, var.resource_tags)
 }
 
 ################################################################################
 # Concourse ALB Target Group
 ################################################################################
-resource "aws_lb_target_group" "concourse_alb_tg" {
-  name     = "concourse-alb-tg"
+resource "aws_lb_target_group" "concourse_lb_tg" {
+  name     = "concourse-lb-tg"
   port     = 443
   protocol = "HTTPS"
   vpc_id   = var.vpc_id
-  tags     = merge({Name = "concourse-alb-tg"}, var.resource_tags)
+  tags     = merge({Name = "concourse-lb-tg"}, var.resource_tags)
   health_check {
     path = "/"
     protocol = "HTTPS"
@@ -49,44 +49,44 @@ resource "aws_lb_target_group" "concourse_alb_tg" {
 #    instance_group = "concourse"
 #  }
 #}
-#resource "aws_lb_target_group_attachment" "concourse_alb_tga" {
+#resource "aws_lb_target_group_attachment" "concourse_lb_tga" {
 #  count            = length(data.aws_instances.concourse_instances.ids)
 #  target_id        = data.aws_instances.concourse_instances.ids[count.index]
-#  target_group_arn = aws_lb_target_group.concourse_alb_tg.arn
+#  target_group_arn = aws_lb_target_group.concourse_lb_tg.arn
 #  port             = 443
 #}
 
 ################################################################################
 # Concourse ALB Listeners - concourse API - HTTPS
 ################################################################################
-resource "aws_alb_listener" "concourse_alb_listener_443" {
-  load_balancer_arn = aws_lb.concourse_alb.arn
+resource "aws_alb_listener" "concourse_lb_listener_443" {
+  load_balancer_arn = aws_lb.concourse_lb.arn
   port = "443"
   protocol = "HTTPS"
   ssl_policy = "ELBSecurityPolicy-TLS-1-2-2017-01"
   certificate_arn = var.concourse_acm_arn
   default_action {
     type = "forward"
-    target_group_arn = aws_lb_target_group.concourse_alb_tg.arn
+    target_group_arn = aws_lb_target_group.concourse_lb_tg.arn
   }
-  tags = merge({Name = "concourse-alb-listener-443"}, var.resource_tags)
+  tags = merge({Name = "concourse-lb-listener-443"}, var.resource_tags)
 }
 
 
 ################################################################################
 # concourse ALB Route53 DNS
 ################################################################################
-resource "aws_route53_record" "concourse_alb_record" {
+resource "aws_route53_record" "concourse_lb_record" {
 
   count   = var.enable_route_53
   zone_id = var.route53_zone_id
   name    = var.concourse_domain
   type    = "CNAME"
   ttl     = "60"
-  records = ["${aws_lb.concourse_alb.dns_name}"]
+  records = ["${aws_lb.concourse_lb.dns_name}"]
 }
 
 
-output "dns_name" {value = aws_lb.concourse_alb.dns_name}
-output "lb_name"  {value = aws_lb.concourse_alb.name }
-output "lb_target_group_name" { value = aws_lb_target_group.concourse_alb_tg.name }
+output "dns_name" {value = aws_lb.concourse_lb.dns_name}
+output "lb_name"  {value = aws_lb.concourse_lb.name }
+output "lb_target_group_name" { value = aws_lb_target_group.concourse_lb_tg.name }
